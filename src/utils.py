d306309eb6b03bf1a7d37051081979ec5529c4a1
MarkH0705 <markus.harak@gmail.com>
2025-03-03 03:57:23 +0000

    🚀 Automatische Aktualisierung des SEO-Projekts

--- src/utils.py
+++ src/utils.py
@@ -14,10 +14,18 @@ def load_prompts(file_path):
         print(f"❌ Fehler: Die Datei {file_name} enthält ungültiges JSON.")
         return {}
 
-def generate_llm_text(prompts, agents, **kwargs):
+def generate_llm_text_streaming(prompts, agents, **kwargs):
     system_prompt = prompts[agents]["system_prompt"]
     user_prompt = prompts[agents]["user_prompt"].format(**kwargs)
 
     cb = Chatbot(system_prompt, user_prompt)
     response = cb.chat_with_streaming()
+    return response
+
+def generate_llm_text(prompts, agents, **kwargs):
+    system_prompt = prompts[agents]["system_prompt"]
+    user_prompt = prompts[agents]["user_prompt"].format(**kwargs)
+
+    cb = Chatbot(system_prompt, user_prompt)
+    response = cb.chat()
     return response
