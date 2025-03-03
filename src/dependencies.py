8a7ee83194824e9e01959838682da7074c0459ca
MarkH0705 <markus.harak@gmail.com>
2025-03-03 16:51:07 +0000

    🚀 Automatische Aktualisierung des SEO-Projekts

--- src/dependencies.py
+++ src/dependencies.py
@@ -1,48 +1,36 @@
 from google.colab import userdata, drive
+
 import os
 import requests
+import re
+import json
+import importlib
+import time
+from collections import Counter
+import subprocess
+import sys
+import string
+import seaborn as sns
+import asyncio
 
 from bs4 import BeautifulSoup, Comment
 from urllib.parse import urljoin, urlparse
 import chardet
 import dotenv
+import ast
+import nltk
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+from wordcloud import WordCloud
+from jinja2 import Template
+from playwright.async_api import async_playwright
+import spacy
+import pypandoc
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
 import openai
-
-import time
-
-import re
-import json
-
-from collections import Counter
-import subprocess
-import sys
-
-import importlib
-
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-
-import ast
-
-
-
-import nltk
-from nltk.corpus import stopwords
-nltk.download('stopwords')
-
-import string
-
-import seaborn as sns
-
-from wordcloud import WordCloud
-
-from jinja2 import Template
-
-import asyncio
-from playwright.async_api import async_playwright
-import spacy
-import pypandoc
